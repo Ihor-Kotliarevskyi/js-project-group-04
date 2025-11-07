9af955edc97581a61d4dd2e125732acef7150134
KateB713 <kbairachnaya@gmail.com>
2025-11-07 15:12:07 +0200

    add js functions

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,40 @@
-// Функції модальних вікон (відкриття, закриття і так далі).
+
+
+// order-modal
+
+const backdropOrderModal = document.querySelector('.backdrop');
+const modalOrder = document.querySelector('.order-modal');
+const closeOrderBtn = document.querySelector('.modal-close-btn');
+const submitBtn = document.querySelector('.modal-submit-btn');
+
+function openOrderModal() {
+    backdropOrderModal.classList.add('is-open');
+    document.body.classList.add('modal-open');
+
+    window.addEventListener('keydown', handleEscape);
+}
+
+function closeOrderModal() {
+    backdropOrderModal.classList.remove('is-open');
+    document.body.classList.remove('modal-open');
+
+    window.removeEventListener('keydown', handleEscape);
+}
+
+closeOrderBtn.addEventListener('click', closeOrderModal);
+
+backdropOrderModal.addEventListener('click', (event) => {
+    if (event.target === backdropOrderModal) {
+        closeOrderModal();
+    }
+});
+
+function handleEscape(event) {
+    if (event.key === 'Escape' || event.key === 'Esc') {
+        closeOrderModal();
+    }
+}
+
+
+// тестова кнопка для відкриття модалки
+document.querySelector('.open-modal-btn')?.addEventListener('click', openOrderModal);
