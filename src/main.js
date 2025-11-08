876c45ba272b51fbba1bdb28fdd415b42862142a
VladuslavFlorko1 <florkovlad@gmail.com>
2025-11-08 18:28:44 +0200

    open_moal_products_and_animation_clouses_buton

--- src/main.js
+++ src/main.js
@@ -14,3 +14,22 @@ window.addEventListener('load', () => {
 document.addEventListener('DOMContentLoaded', initialHome);
 refs.categories.addEventListener('click', handlerClickCategory);
 refs.loadMoreBtn.addEventListener('click', onLoadMoreClick);
+import { openProductModal } from './js/modal.js';
+
+const grid = document.getElementById('products');
+
+if (grid) {
+  grid.addEventListener('click', e => {
+    const trigger = e.target.closest('.products-details-btn, .products-image');
+    if (!trigger) return;
+    const card = trigger.closest('.product-item');
+    if (!card) return;
+    const id = card.dataset.id; //
+    if (!id) {
+      console.warn('Не знайдено data-id на .product-item');
+      return;
+    }
+    e.preventDefault(); 
+    openProductModal(id);
+  });
+}
